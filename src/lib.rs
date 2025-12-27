bf10a31bcf5d494e5001c3e2843576afacff0aa0
Praphulla06 <praphullapradhananga815@gmail.com>
2025-12-27 18:21:24 +0545

    Implemented command parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,32 @@
-use std::env;
+use std::{env, fs::File, io};
 
-pub fn parse_input() {
+pub fn parse_input() -> Result<(String, File), io::Error> {
     let args: Vec<String> = env::args().collect();
+
+    let pattern = match args.get(1) {
+        Some(p) => p.to_string(),
+        None => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Enter a pattern!",
+            ));
+        }
+    };
+
+    let file: File = match args.get(2) {
+        Some(filename) => match File::open(filename) {
+            Ok(file) => file,
+            Err(e) => {
+                return Err(io::Error::new(io::ErrorKind::InvalidInput, e));
+            }
+        },
+        None => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidFilename,
+                "Enter a filename",
+            ));
+        }
+    };
+
+    Ok((pattern, file))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use mini_grep::parse_input;
+
 fn main() {
-    
+    let (pattern, file) = match parse_input() {
+        Ok((p, f)) => (p, f),
+        Err(e) => {
+            println!("{}", e);
+            return; 
+        }
+    };
+
+    println!("{}", pattern);
+    println!("{:#?}", file);
 }
